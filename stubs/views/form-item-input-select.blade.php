fa0b81f3fc262aa155787b515c3a7ba8d71c12bd
thorvh <57983409+thorvh@users.noreply.github.com>
2024-08-25 12:52:45 +0200

    Add files via upload

--- stubs/views/form-item-input-select.blade.php
+++ stubs/views/form-item-input-select.blade.php
@@ -3,6 +3,7 @@
 	<option value="" selected>{{$item}}</option>
 	@foreach ($item_array as $item)
 		<option value="{{ $item->id }}">{{ $item->name }}</option>
+		 <option {{old('$item',$old_value)==$item->id? 'selected':''}} value="{{ $item->id }}">{{ $item->name }}</option>
 	@endforeach
 </select>
 
